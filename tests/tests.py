f570b021af98a73f97bb157ec98836cdb7833538
Lorence Cramwinckel <lorcr@live.nl>
2020-12-21 15:09:56 +0100

    Create tests.py

---
+++ tests/tests.py
@@ -0,0 +1,11 @@
+def test_correct_addition():
+    assert Calculator(2, 2).add == 4
+
+def test_correct_subtraction():
+    assert Calculator(3, 1).subtract == 2
+
+def test_correct_division():
+    assert Calculator(10, 2).divide == 5
+
+def test_correct_multiplication():
+    assert Calculator(10, 2).multiply == 20
