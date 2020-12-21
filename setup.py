f3278e983ade95068f661dea33ec02532d646e52
Lorence Cramwinckel <lorcr@live.nl>
2020-12-21 15:08:22 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='calculator',
+    version='0.0.1',
+    description='A simple calculator package',
+    py_modules=["calculator"],
+    package_dir={'': 'src'},
+)
