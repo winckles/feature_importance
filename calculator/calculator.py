1a00e6d2a492a4225e4675f8a5aa2edab9c14e45
Lorence Cramwinckel <lorencecramwinckel@MacBook-Pro.local>
2020-12-22 15:43:24 +0100

    styled files according to pep8

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,5 +1,5 @@
 class Calculator:
-  def __init__(self, number_1: int, number_2: int):
+    def __init__(self, number_1: int, number_2: int):
         self.__number_1 = number_1
         self.__number_2 = number_2
         self.__memory = 0
@@ -9,18 +9,16 @@ class Calculator:
         return self.__number_1
 
     @property
-    def numer_2(self) -> int:
+    def number_2(self) -> int:
         return self.__number_2
 
     @property
     def memory(self) -> int:
         return self.__memory
 
-
     def check_memory(self) -> str:
         """ Check the number in memory """
         return f"The number in memory is {self.__memory}%"
-
 
     def reset_memory(self):
         """ Resets the memory back to 0 """
@@ -28,23 +26,19 @@ class Calculator:
         if self.__memory is not 0:
             self.__memory = 0
 
-
     def add(self, number_1, number_2) -> int:
         """ Returns number_1 + number_2 """
         return number_1 + number_2
 
-    
     def subtract(self, number_1, number_2) -> int:
         """ Returns number_1 - number_2 """
         return number_1 - number_2
-
 
     def multiply(self, number_1, number_2) -> int:
         """ Returns number_1 * number_2 """
         return number_1 * number_2
 
-
     def divide(self, number_1, number_2) -> int:
         """ Returns number_1 / number_2 """
         return number_1 / number_2
-    
+
