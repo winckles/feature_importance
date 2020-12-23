379caf353d4041ad1cf32bc527f1b3928d22d25b
winckles <lorcr@live.nl>
2020-12-23 12:58:15 +0100

    Rename file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='calculator',
-    version='0.0.1',
+    version='0.0.2',
     author="Lorence Cramwinckel",
     author_email="lorcr@live.nl",
     description='A simple calculator package',
     url="https://github.com/winckles/calculator_package",
-    py_modules=["calculator"],
+    py_modules=["calculator_package"],
     package_dir={'': 'calculator'},
     python_requires='>=3.6',
 )
